9b1509867ff66c378fe0850dd36996ac32d972bb
Qingshan Luo <edoger@qq.com>
2020-10-17 11:27:30 +0800

    Add tests.

--- task_test.go
+++ task_test.go
@@ -15,7 +15,11 @@
 package runner
 
 import (
+	"io"
+	"io/ioutil"
+	"net/http"
 	"testing"
+	"time"
 )
 
 func TestNewTaskFromFunc(t *testing.T) {
@@ -23,14 +27,8 @@ func TestNewTaskFromFunc(t *testing.T) {
 
 	tasks = []Task{
 		NewTaskFromFunc(nil),
-		NewTaskFromFunc(func() error {
-			return nil
-		}),
-		NewTaskFromFunc(func() error {
-			return nil
-		}, func() error {
-			return nil
-		}),
+		NewTaskFromFunc(func() error { return nil }),
+		NewTaskFromFunc(func() error { return nil }, func() error { return nil }),
 	}
 	for _, task := range tasks {
 		if task == nil {
@@ -54,5 +52,63 @@ func TestNewTaskFromFuncPanic(t *testing.T) {
 
 	if v == nil {
 		t.Fatal("NewTaskFromFunc(nil, nil, nil): no panic")
+	}
+}
+
+func TestNewTaskFromHTTPServer(t *testing.T) {
+	task := NewTaskFromHTTPServer(new(http.Server), nil)
+	if task == nil {
+		t.Fatal("NewTaskFromHTTPServer(): nil task")
+	}
+}
+
+func TestNewTaskFromHTTPServerPanic(t *testing.T) {
+	var v interface{}
+	func() {
+		defer func() { v = recover() }()
+		NewTaskFromHTTPServer(nil, nil)
+	}()
+
+	if v == nil {
+		t.Fatal("NewTaskFromHTTPServer(): no panic")
+	}
+}
+
+func TestHTTPServerTask(t *testing.T) {
+	addr := "127.0.0.1:38281"
+	task := NewTaskFromHTTPServer(&http.Server{
+		Addr: addr,
+		Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			_, _ = io.WriteString(w, "ok")
+		}),
+	}, nil)
+
+	r := New()
+	if err := r.Run(task); err != nil {
+		t.Fatal(err)
+	}
+	time.Sleep(time.Millisecond * 20)
+
+	if res, err := http.Get("http://" + addr); err != nil {
+		t.Fatal(err)
+	} else {
+		data, err := ioutil.ReadAll(res.Body)
+		_ = res.Body.Close()
+		if err != nil {
+			t.Fatal(err)
+		}
+		if s := string(data); s != "ok" {
+			t.Fatalf("The http server response: %s", s)
+		}
+	}
+
+	if err := r.Exit(); err != nil {
+		t.Fatal(err)
+	}
+	if err := task.Shutdown(); err != nil {
+		t.Fatal(err)
+	}
+	if err := task.Execute(); err != http.ErrServerClosed {
+		t.Fatalf("The http server task not closed: %s", err)
 	}
 }
